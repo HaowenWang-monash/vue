05f8085d36e5ab6f96c996bb3ec9142ac96363d4
HaowenWang-monash <hwan0271@student.monash.edu>
2025-03-16 11:28:29 +0800

    Update vite.config.js

--- vue-app/vite.config.js
+++ vue-app/vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
 
 export default defineConfig({
-  base: "./", 
+  base: "/",  // 确保路径正确
   plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src')
-    }
-  },
   build: {
-    outDir: "dist", 
-  }
+    outDir: "dist",  // 指定构建目录
+  },
 });
